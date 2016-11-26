1934f933730ee85a2ca76a10f6049ebcdf494b5a
Aykut <aykut@Aykut-MacBook-Air.local>
2016-11-26 22:39:18 +0200

    fed3

--- CancerCareNew/AboutViewController.swift
+++ CancerCareNew/AboutViewController.swift
@@ -1,17 +1,14 @@
 //
-//  HomePageViewController.swift
+//  AboutViewController.swift
 //  CancerCareNew
 //
-//  Created by SOS Z14-12 on 26/11/16.
+//  Created by Aykut on 26.11.2016.
 //  Copyright © 2016 FED. All rights reserved.
 //
 
 import UIKit
 
-class HomePageViewController: UIViewController {
-    
-    @IBOutlet weak var menuButton: UIButton!
-    var storyboardRef = UIStoryboard(name: "Main", bundle: nil)
+class AboutViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,17 +21,6 @@ class HomePageViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    /*
-    @IBAction func menuButtonAction(_ sender: Any) {
-        let nextTableViewController = storyboardRef.instantiateViewController(withIdentifier: "menuTablePage") as! MenuTableViewController
-        self.present(nextTableViewController, animated: true)
-        /*
-        instantiateViewController(withIdentifier: "loginPage") as! LoginViewController
-        */
-        
-        //self.present(nextViewController, animated: true)
-    }
-     */
 
     /*
     // MARK: - Navigation
