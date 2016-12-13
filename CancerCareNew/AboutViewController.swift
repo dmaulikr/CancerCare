c6312d5cf23f01e3252369c1f186db3b0e174136
SOS Z14-12 <sosz14-12@SOSs-iMac-3.local>
2016-12-13 23:26:43 +0200

    14December

--- CancerCareNew/AboutViewController.swift
+++ CancerCareNew/AboutViewController.swift
@@ -10,10 +10,16 @@ import UIKit
 
 class AboutViewController: UIViewController {
 
+    @IBOutlet weak var infoTextView: UITextView!
+    @IBOutlet weak var homePageLinkButton: UIButton!
+    @IBOutlet weak var donateButton: UIButton!
+    @IBOutlet weak var contactButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.title = "Hakkımızda"
         self.navigationItem.title = self.navigationController?.title
+        
         // Do any additional setup after loading the view.
     }
 
@@ -21,8 +27,43 @@ class AboutViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
+    @IBAction func toHomePageLink(_ sender: UIButton) {
+        //http://www.kacuv.org/
+        if let url = NSURL(string: "http://www.kacuv.org/") {
+            UIApplication.shared.openURL(url as URL)
+        }
+    }
+    
+    @IBAction func toDonateLink(_ sender: UIButton) {
+        //https://bagis.kacuv.org/#/
+        if let url = NSURL(string: "https://bagis.kacuv.org/#/") {
+            UIApplication.shared.openURL(url as URL)
+        }
+    }
+    
+    @IBAction func toContactLink(_ sender: UIButton) {
+        //http://www.kacuv.org/iletisim/
+        if let url = NSURL(string: "http://www.kacuv.org/iletisim/") {
+            UIApplication.shared.openURL(url as URL)
+        }
+    }
+
+    
+    /*
+ @IBAction func toHomePageLink(_ sender: UIButton) {
+ 
+ // http://www.kacuv.org/
+ 
+ if let url = NSURL(string: "http://www.kacuv.org/") {
+ 
+ UIApplication.shared.openURL(url as URL)
+ 
+ }
+ 
+ }
+ */
+ 
     /*
     // MARK: - Navigation
 
--- CancerCareNew/HomePageViewController.swift
+++ CancerCareNew/HomePageViewController.swift
@@ -24,6 +24,7 @@ class HomePageViewController: UIViewController {
     let storageRef = FIRStorage.storage().reference()
     let networkingService = NetworkingService()
     let currUser = FIRAuth.auth()?.currentUser
+    var isAddEnabled = false
 
     
     @IBOutlet weak var menuButton: UIButton!
@@ -31,12 +32,41 @@ class HomePageViewController: UIViewController {
 
     
     @IBAction func saveMoodAction(_ sender: Any) {
-        let moodToBeSaved = Int(moodSlider.value).description
-        networkingService.updateMood(user: currUser!, mood: moodToBeSaved)
+        if isAddEnabled {
+            saveMoodButton.setTitle("Ekle", for: .normal)
+            self.moodSlider.isHidden = true
+            let moodToBeSaved = Int(moodSlider.value).description
+            networkingService.updateMood(user: currUser!, mood: moodToBeSaved)
+            moodTextLabel.text = moodToBeSaved
+            isAddEnabled = false
+        } else {
+            saveMoodButton.setTitle("Kaydet", for: .normal)
+            self.moodSlider.isHidden = false
+            isAddEnabled = true
+        }
     }
     
     @IBAction func moodSliderChanged(_ sender: Any) {
-        moodTextLabel.text = "Çocuğumun bugünkü durumu (10 üzerinden): " + Int(moodSlider.value).description
+        let moodInt = Int(moodSlider.value)
+        let moodString = moodInt.description
+ 
+        if moodInt == 1 {
+            moodSlider.tintColor = UIColor.blue
+            moodTextLabel.text = moodString
+        } else if moodInt == 2 {
+            moodSlider.tintColor = UIColor.green
+            moodTextLabel.text = moodString
+        } else if moodInt == 3 {
+            moodSlider.tintColor = UIColor.yellow
+            moodTextLabel.text = moodString
+        } else if moodInt == 4 {
+            moodSlider.tintColor = UIColor.orange
+            moodTextLabel.text = moodString
+        } else {
+            moodSlider.tintColor = UIColor.red
+            moodTextLabel.text = moodString
+        }
+        // moodTextLabel.text = "Çocuğumun bugünkü durumu (10 üzerinden): " + Int(moodSlider.value).description
     }
     
     override func viewDidLoad() {
@@ -44,7 +74,12 @@ class HomePageViewController: UIViewController {
         self.navigationController?.title = "Ana Sayfa"
         self.navigationItem.title = self.navigationController?.title
         moodTextLabel.text = Int(moodSlider.value).description
-
+        // moodSlider stuff
+        self.isAddEnabled = false
+        self.saveMoodButton.setTitle("Ekle", for: .normal)
+        self.moodSlider.isHidden = true
+        //
+        
         /*
         let currUser = FIRAuth.auth()?.currentUser
         let userID = currUser?.uid
